ca45ebed96d15f63fdb3cea65a84e77f457bc2b1
Serjih Sklovski <serjihsklovski@gmail.com>
2017-01-30 18:56:51 +0300

    'User' model

---
+++ src/main/java/model/User.java
@@ -0,0 +1,32 @@
+package model;
+
+public class User {
+
+    private long id;
+    private String login;
+    private String email;
+    private String password;
+
+    public User(long id, String login, String email, String password) {
+        this.id = id;
+        this.login = login;
+        this.email = email;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
