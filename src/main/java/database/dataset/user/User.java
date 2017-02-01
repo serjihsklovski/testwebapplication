06a95cddcb5b695b07456ab1f85da9deb80a4332
Serjih Sklovski <serjihsklovski@gmail.com>
2017-02-01 16:38:57 +0300

    added: setters

--- src/main/java/database/dataset/user/User.java
+++ src/main/java/database/dataset/user/User.java
@@ -30,6 +30,22 @@ public class User {
         return password;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public String toString() {
         return String.format("<user id=\"%d\" login=\"%s\" email=\"%s\" password=\"%s\" />",
