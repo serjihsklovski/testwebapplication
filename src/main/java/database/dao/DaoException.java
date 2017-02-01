d025acc052a649f7e52cc32ab274923f0f1b29b3
Serjih Sklovski <serjihsklovski@gmail.com>
2017-02-02 02:36:27 +0300

    exception for all dao methods

---
+++ src/main/java/database/dao/DaoException.java
@@ -0,0 +1,18 @@
+package database.dao;
+
+import database.DataBaseException;
+
+public class DaoException extends DataBaseException {
+
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(Throwable cause) {
+        super(cause);
+    }
+
+    public DaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
