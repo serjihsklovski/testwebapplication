e10dfec9d31b1037175eb74533af23fe5ed7e11e
Serjih Sklovski <serjihsklovski@gmail.com>
2017-02-02 04:58:41 +0300

    extends 'database.DataBaseException'

--- src/main/java/database/DataBaseServiceException.java
+++ src/main/java/database/DataBaseServiceException.java
@@ -1,6 +1,6 @@
 package database;
 
-public class DataBaseServiceException extends Exception {
+public class DataBaseServiceException extends DataBaseException {
     public DataBaseServiceException(Exception cause) {
         super(cause);
     }
