6a9b0a469edbfb74a2fe55b9fe35fc96dcae8abe
serjihsklovski <serjihsklovski@gmail.com>
2017-02-10 18:22:21 +0300

    new interface: 'helper.executor.TransactionBody' of 'java.util.concurrent.Callable'. its method 'call' throws 'java.sql.SQLException', not 'java.lang.Exception'

---
+++ src/main/java/helper/executor/TransactionBody.java
@@ -0,0 +1,10 @@
+package helper.executor;
+
+import java.sql.SQLException;
+import java.util.concurrent.Callable;
+
+public interface TransactionBody<T> extends Callable<T> {
+
+    @Override
+    T call() throws SQLException;
+}
