768c406696ec3135ea2bfb9f28ce0ebe9d8c7d96
serjihsklovski <serjihsklovski@gmail.com>
2017-02-10 23:04:25 +0300

    adapting adstract filter

---
+++ src/main/java/filter/AbstractFilter.java
@@ -0,0 +1,31 @@
+package filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public abstract class AbstractFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // NOP
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+
+        doFilter((HttpServletRequest) request, (HttpServletResponse) response, chain);
+    }
+
+    public abstract void doFilter(HttpServletRequest request,
+                                  HttpServletResponse response,
+                                  FilterChain chain)
+            throws IOException, ServletException;
+
+    @Override
+    public void destroy() {
+        // NOP
+    }
+}
