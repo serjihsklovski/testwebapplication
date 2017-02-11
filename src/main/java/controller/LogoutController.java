989be86280bbf80bdf80fddf1f230eb78b543d00
serjihsklovski <serjihsklovski@gmail.com>
2017-02-11 10:37:13 +0300

    new controller: 'controller.LogoutController'

---
+++ src/main/java/controller/LogoutController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=utf-8");
+        request.setCharacterEncoding("UTF-8");
+
+        request.getSession().invalidate();
+        response.sendRedirect("/home");
+    }
+}
